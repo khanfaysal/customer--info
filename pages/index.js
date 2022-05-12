8c12254bbe48e6adf3a305aaa02cd398f335e15e
lincoln khan <mdfaysalkhancse@gmail.com>
2022-05-12 10:33:03 +0600

    login form customize

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,20 @@
-import { Button, Flex, Heading, Input } from "@chakra-ui/react";
+import { Button, Flex, FormControl, FormLabel, Heading, Input } from "@chakra-ui/react";
 
 const IndexPage = () => {
     return (
-        <Flex height="100vh" alignItems="center" justifyContent="center">
-            <Flex direction="column" background="gray.100" padding={12} rounded={6}>
-                <Heading mb={6}>Login</Heading>
-                <Input type="email" placeholder="faysalkhan@gmail.com" variant="filled" mb={3} />
-                <Input type="password" placeholder="*******" variant="filled" mb={6} />
-                <Button colorScheme="blue">Login</Button>
-            </Flex>
+        <FormControl>
+            <Flex height="100vh" alignItems="center" justifyContent="center">
+                <Flex direction="column" background="gray.100" padding={12} rounded={6}>
+                    <Heading mb={6}>Login</Heading>
+                    <FormLabel htmlFor='email'>Email address</FormLabel>
+                    <Input type="email" id="email" placeholder="faysalkhan@gmail.com" mb={3} />
+                    <FormLabel htmlFor='password'>Password</FormLabel>
+                    <Input type="password" id="password" placeholder="*******" mb={6} />
+                    <Button colorScheme="blue">Login</Button>
+                </Flex>
 
-        </Flex>
+            </Flex>
+        </FormControl>
     )
 }
 export default IndexPage;
