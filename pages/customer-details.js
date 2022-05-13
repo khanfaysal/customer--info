0bcff6c5253832af36df0f4bffe58e89e1dac003
lincoln khan <mdfaysalkhancse@gmail.com>
2022-05-13 16:39:15 +0600

    data handle running on customer details form

--- pages/customer-details.js
+++ pages/customer-details.js
@@ -2,24 +2,42 @@ import { useState } from "react";
 import { Box, Button, Center, Checkbox, FormControl, FormLabel, Heading, HStack, Input, Link, Radio, RadioGroup, Stack, Text, Textarea, VStack } from "@chakra-ui/react";
 import NextLink from "next/link";
 
+// initial form data
+
+const InitialFormData = {
+    firstName: "",
+    lastName: "",
+    customer__strAddr: "",
+    customer__strAddr1: "",
+    customer__city: "",
+    customer__state: "",
+    customer__phone: "",
+    customer__email: "",
+    gender: "",
+    customer__age: ""
+}
+
 const CustomerDetails = () => {
 
-    const [value, setValue] = useState('male');
+    // /form data managing states
+    const [formData, setFormData] = useState(InitialFormData);
+    const [errorMessage, setErrorMessage] = useState(formData);
 
-    // initial form data
 
-    const InitialFormData = {
-        firstName: "",
-        lastName: "",
-        customer__strAddr: "",
-        customer__strAddr1: "",
-        customer__city: "",
-        customer__state: "",
-        customer__phone: "",
-        customer__email: "",
-        gender: "",
-        customer__age: ""
+    // input handler
+    const onChangeHandler = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+
     }
+
+    //on submit handler
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+        setErrorMessage(validator(formData));
+    }
+
+
 
     return (
         <Box
@@ -48,12 +66,12 @@ const CustomerDetails = () => {
                 <HStack justify='space-between' w="full">
 
                     <FormControl isRequired>
-                        <Input type="text" id="firstName" name="customer__firstName" mb={2} />
+                        <Input type="text" id="firstName" name="customer__firstName" onChange={onChangeHandler} mb={2} />
                         <FormLabel htmlFor='firstName' color="gray.600" fontSize='sm' fontWeight="400">First Name</FormLabel>
                     </FormControl>
 
                     <FormControl isRequired>
-                        <Input type="text" id="lastName" name="customer__lastName" mb={2} />
+                        <Input type="text" id="lastName" name="customer__lastName" onChange={onChangeHandler} mb={2} />
                         <FormLabel htmlFor='lastName' color="gray.600" fontSize='sm' fontWeight="400">Second Name</FormLabel>
                     </FormControl>
 
@@ -63,24 +81,24 @@ const CustomerDetails = () => {
                 <Text fontWeight="600">Address</Text>
 
                 <FormControl isRequired>
-                    <Input type="text" id="streetAddress" name="customer__strAddr" mb={2} />
+                    <Input type="text" id="streetAddress" name="customer__strAddr" onChange={onChangeHandler} mb={2} />
                     <FormLabel htmlFor='streetAddress' color="gray.600" fontSize='sm' fontWeight="400">Street Address</FormLabel>
                 </FormControl>
 
                 <FormControl isRequired>
-                    <Input type="text" id="streetAddress1" name="customer__strAddr1" mb={2} />
+                    <Input type="text" id="streetAddress1" name="customer__strAddr1" onChange={onChangeHandler} mb={2} />
                     <FormLabel htmlFor='streetAddress1' color="gray.600" fontSize='sm' fontWeight="400">Street Address 1</FormLabel>
                 </FormControl>
 
                 {/* city and state section */}
                 <HStack justify='space-between' w="full">
                     <FormControl isRequired>
-                        <Input type="text" id="city" name="customer__city" mb={2} />
+                        <Input type="text" id="city" name="customer__city" onChange={onChangeHandler} mb={2} />
                         <FormLabel htmlFor='city' color="gray.600" fontSize='sm' fontWeight="400">City</FormLabel>
                     </FormControl>
 
                     <FormControl isRequired>
-                        <Input type="text" id="province" name="customer__state" mb={2} />
+                        <Input type="text" id="province" name="customer__state" onChange={onChangeHandler} mb={2} />
                         <FormLabel htmlFor='province' color="gray.600" fontSize='sm' fontWeight="400">Province/State</FormLabel>
                     </FormControl>
 
@@ -90,12 +108,12 @@ const CustomerDetails = () => {
                 <Text fontWeight="600">Phone Number And Email</Text>
                 <HStack justify='space-between' w="full">
                     <FormControl isRequired>
-                        <Input type="text" id="phoneNumber" name="customer__phone" mb={2} />
+                        <Input type="text" id="phoneNumber" name="customer__phone" onChange={onChangeHandler} mb={2} />
                         <FormLabel htmlFor='phoneNumber' color="gray.600" fontSize='sm' fontWeight="400">Phone Number</FormLabel>
                     </FormControl>
 
                     <FormControl isRequired>
-                        <Input type="email" id="email" name="customer__email" mb={2} />
+                        <Input type="email" id="email" name="customer__email" onChange={onChangeHandler} mb={2} />
                         <FormLabel htmlFor='email' color="gray.600" fontSize='sm' fontWeight="400">Email</FormLabel>
                     </FormControl>
 
@@ -105,16 +123,16 @@ const CustomerDetails = () => {
                 <Text fontWeight="600">Gender And Age</Text>
                 <HStack justify='space-between' gap={6} w="full">
 
-                    <RadioGroup onChange={setValue} value={value}>
+                    <RadioGroup onChange={onChangeHandler}>
                         <Stack direction='row'>
                             <Radio name="gender" value='male' id="male">Male</Radio>
-                            <Radio name="gender" value='female' id="female">Female</Radio>
-                            <Radio name="gender" value='others' id="others">Others</Radio>
+                            <Radio name="gender" value='female' id="female" >Female</Radio>
+                            <Radio name="gender" value='others' id="others" >Others</Radio>
                         </Stack>
                     </RadioGroup>
 
                     <FormControl isRequired>
-                        <Input type="number" id="number" name="customer__age" mb={2} />
+                        <Input type="number" id="number" name="customer__age" onChange={onChangeHandler} mb={2} />
                         <FormLabel htmlFor='number' color="gray.600" fontSize='sm' fontWeight="400">Age</FormLabel>
                     </FormControl>
 
@@ -123,7 +141,7 @@ const CustomerDetails = () => {
                 {/* text area section */}
                 <Textarea />
 
-                <Button mb={3} colorScheme="blue">Sign up</Button>
+                <Button onClick={onSubmitHandler} mb={3} colorScheme="blue">Sign up</Button>
                 <Center>
                     <p>Already a member?
 
@@ -138,3 +156,29 @@ const CustomerDetails = () => {
     )
 }
 export default CustomerDetails;
+
+const validator = (data) => {
+
+    //regexes
+    const regexForEmail = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const regexForName = /^[a-zA-Z ]{2,30}$/;
+
+    // error obejct
+    let err = {};
+
+    //email validation
+    if (!data.email.trim()) {
+        err.email = "Email is required!";
+    } else if (!regexForEmail.test(data.email)) {
+        err.email = "Email is not in valid format!"
+    }
+
+    //password validation
+    if (!data.password.trim()) {
+        err.password = "Password is required!";
+    } else if (data.password < 8) {
+        err.password = "Password is not at least 8 charecters!"
+    }
+
+    return err;
+}
